d5d16f8cbd3325b543d5ffda1c97948f7066d83c
levent.uysal <levent.uysal@algomedi.com>
2021-11-14 00:53:51 +0300

    update

---
+++ Bitir.Mobile/Bitir.Mobile/Converters/OrderStatusColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using Core.Enums;
+using Module.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Bitir.Mobile.Converters
+{
+    public class OrderStatusColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (OrderStatusEnum)value;
+            switch (status)
+            {
+                case OrderStatusEnum.InCarrier:
+                    return Color.FromHex("#fffdbc");
+                case OrderStatusEnum.Completed:
+                    return Color.FromHex("#bcffd7");
+                default:
+                    return Color.White;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Bitir.Mobile/Bitir.Mobile/Converters/OrderStatusMessageConverter.cs
@@ -0,0 +1,29 @@
+﻿using Module.Shared.Enums;
+using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Bitir.Mobile.Converters
+{
+    public class OrderStatusMessageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (OrderStatusEnum)value;
+            switch (status)
+            {
+                case OrderStatusEnum.InCarrier:
+                    return "Sipariş Taşıma Durumunda";
+                case OrderStatusEnum.Completed:
+                    return "Sipariş Tamamlandı";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
